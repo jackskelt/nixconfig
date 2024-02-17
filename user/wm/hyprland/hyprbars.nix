ee143abffdb1d4c15160da44dacb966a1cbb3324
JackSkelt <davedossantos978@gmail.com>
2024-02-17 13:04:44 -0300

    split into files

--- user/wm/hyprland/hyprbars.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, pkgs, hyprland-plugins, ...}:
-
-stdenv.mkDerivation  {
-    pname = "hyprbard";
-    version = "stable";
-    src = "${hyprland-plugins}/hyprbars";
-    nativeBuildInputs = [ pkgs.hyprland.nativeBuildInputs ];
-    buildInputs = [ pkgs.hyprland pkgs.hyprland.buildInputs ];
-}
---
+++ user/wm/hyprland/screen_locker/swaylock.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  
+  home.file.".config/gtklock/style.css".text = ''
+    window {
+      background-image: url("''+config.stylix.image+''");
+      background-size: auto 100%;
+    }
+  '';
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
+    settings = {
+      color = "#"+config.lib.stylix.colors.base00;
+      inside-color = "#"+config.lib.stylix.colors.base00+"cc";
+      inside-caps-lock-color = "#"+config.lib.stylix.colors.base09;
+      inside-clear-color = "#"+config.lib.stylix.colors.base0A;
+      inside-wrong-color = "#"+config.lib.stylix.colors.base08;
+      inside-ver-color = "#"+config.lib.stylix.colors.base0D;
+      line-color = "#"+config.lib.stylix.colors.base00;
+      line-caps-lock-color = "#"+config.lib.stylix.colors.base00;
+      line-clear-color = "#"+config.lib.stylix.colors.base00;
+      line-wrong-color = "#"+config.lib.stylix.colors.base00;
+      line-ver-color = "#"+config.lib.stylix.colors.base00;
+      ring-color = "#"+config.lib.stylix.colors.base00;
+      ring-caps-lock-color = "#"+config.lib.stylix.colors.base09;
+      ring-clear-color = "#"+config.lib.stylix.colors.base0A;
+      ring-wrong-color = "#"+config.lib.stylix.colors.base08;
+      ring-ver-color = "#"+config.lib.stylix.colors.base0D;
+      text-color = "#"+config.lib.stylix.colors.base00;
+      key-hl-color = "#"+config.lib.stylix.colors.base0B;
+      font = config.stylix.fonts.monospace.name;
+      font-size = 20;
+      fade-in = 0.5;
+      grace = 5;
+      indicator-radius = 100;
+      screenshots = true;
+      effect-blur = "10x10";
+    };
+  };
+}
