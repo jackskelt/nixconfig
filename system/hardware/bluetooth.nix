56fcf513c8667381348856f0ec1eaa31b218ce49
JackSkelt <davedossantos978@gmail.com>
2024-05-16 13:52:59 -0300

    fix disable bluetooth on startup

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -4,4 +4,7 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false;
   services.blueman.enable = true;
+
+  # Disable bluetooth on startup
+  services.tlp.settings.DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
 }
